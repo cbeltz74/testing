3b91e6cf37a397a173f3d8d3fdc2167782c724ad
Colin Beltz <colinbeltz@gmail.com>
2023-09-09 11:51:27 -0500

    Completed 17-6 Fire events and User Events

--- src/components/__test__/Counter.test.js
+++ src/components/__test__/Counter.test.js
@@ -1,6 +1,7 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import '@testing-library/jest-dom';
 import { Counter } from "../Counter";
+import userEvent from "@testing-library/user-event";
 
 describe("Counter component test", () => {
 
@@ -11,22 +12,75 @@ describe("Counter component test", () => {
     });
     
     test("render the result", () => {
-        const { getByTextId } = render(<Counter />);
-        const charLength = getByTextId("charLength");
+        const { getByTestId } = render(<Counter />);
+        const charLength = getByTestId("charLength");
         expect(charLength.innerHTML).toBe("Character: 0");
     });
 
-    test("render the word result", () => {
-        render(<Counter />);
-        const wordLength = screen.getByTestId("wordLength");
-        expect(wordLength).toBeTruthy();
-    });
+    // test("render the word result", () => {
+    //     render(<Counter />);
+    //     const wordLength = screen.getByTestId("wordLength");
+    //     expect(wordLength).toBeTruthy();
+    // });
 
     test("render the word result", () => {
         render(<Counter />);
         const wordLength = screen.getByText("Word: 0");
         expect(wordLength).toBeInTheDocument();
     });
+
+    test("change textarea and update result", () => {
+        render(<Counter />);
+        const textArea = screen.getByTestId("textArea");
+        const charLength = screen.getByTestId("charLength");
+        const wordLength = screen.getByTestId("wordLength")
+        userEvent.type(textArea, "shubham");
+        expect(charLength.innerHTML).toBe("Character: 7");
+        expect(wordLength.innerHTML).toBe("Word: 1");
+    });
+
+    test("clear textarea and update result", () => {
+        render(<Counter />);
+        const textArea = screen.getByTestId("textArea");
+        const clrBtn = screen.getByTestId("clearBtn");
+        const charLength = screen.getByTestId("charLength");
+        const wordLength = screen.getByTestId("wordLength")
+
+        userEvent.type(textArea, "shubham");
+        userEvent.click(clrBtn);
+        expect(charLength.innerHTML).toBe("Character: 0");
+        expect(wordLength.innerHTML).toBe("Word: 0");
+
+    });
+
+    test("clear textarea and update result all buttons", () => {
+        render(<Counter />);
+        const textArea = screen.getByTestId("textArea");
+        const clrBtn = screen.getAllByTestId("clearBtn");
+        const charLength = screen.getByTestId("charLength");
+        const wordLength = screen.getByTestId("wordLength")
+
+        userEvent.type(textArea, "shubham");
+        userEvent.click(clrBtn[0]);
+        expect(charLength.innerHTML).toBe("Character: 0");
+        expect(wordLength.innerHTML).toBe("Word: 0");
+
+    });
+
+    test("clear textarea and update result all buttons fire event", () => {
+        render(<Counter />);
+        // const textArea = screen.getByTestId("textArea");
+        const clrBtn = screen.getAllByTestId("clearBtn");
+        const charLength = screen.getByTestId("charLength");
+        const wordLength = screen.getByTestId("wordLength")
+
+        // userEvent.type(textArea, "shubham");
+        fireEvent.click(clrBtn[0]);
+        expect(charLength.innerHTML).toBe("Character: 0");
+        expect(wordLength.innerHTML).toBe("Word: 0");
+
+    });
+
 
 });
 
