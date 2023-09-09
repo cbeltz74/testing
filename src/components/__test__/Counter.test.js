61198f1e6912e03b765534b6081942c1ddc8898e
Colin Beltz <colinbeltz@gmail.com>
2023-09-09 11:30:22 -0500

    Completed 17-5

--- src/components/__test__/Counter.test.js
+++ src/components/__test__/Counter.test.js
@@ -1,16 +1,35 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom';
 import { Counter } from "../Counter";
 
+describe("Counter component test", () => {
 
-// test("render the textarea", () => {
-//     const { getByTestId } = render(<Counter />);
-//     const textArea = getByTestId("textArea");
-//     expect(textArea).toBeTruthy();
-// })
+    test("render the textarea", () => {
+        const { getByPlaceholderText } = render(<Counter />);
+        const textArea = getByPlaceholderText("Type or paste your text");
+        expect(textArea).toBeTruthy();
+    });
+    
+    test("render the result", () => {
+        const { getByTextId } = render(<Counter />);
+        const charLength = getByTextId("charLength");
+        expect(charLength.innerHTML).toBe("Character: 0");
+    });
+
+    test("render the word result", () => {
+        render(<Counter />);
+        const wordLength = screen.getByTestId("wordLength");
+        expect(wordLength).toBeTruthy();
+    });
+
+    test("render the word result", () => {
+        render(<Counter />);
+        const wordLength = screen.getByText("Word: 0");
+        expect(wordLength).toBeInTheDocument();
+    });
+
+});
 
 
-test("render the textarea", () => {
-    const { getByPlaceholderText } = render(<Counter />);
-    const textArea = getByPlaceholderText("Type or paste your text");
-    expect(textArea).toBeTruthy();
-})
+
+
